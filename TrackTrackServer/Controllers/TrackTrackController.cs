6dc3900f71088b7690ab3593870644ffe236950b
User <User@LAB3-3>
2023-11-06 12:21:41 +0200

    progress

--- TrackTrackServer/Controllers/TrackTrackController.cs
+++ TrackTrackServer/Controllers/TrackTrackController.cs
@@ -20,5 +20,34 @@ namespace TrackTrackServer.Controllers
         {
             return Ok("hi");
         }
+
+        [Route("AddUser")]
+        [HttpGet]
+        public async Task<ActionResult> AddUser(string name, string password, string email)
+        {
+            try
+            {
+                context.Users.Add(new User { Name = name, Password = password, Email = email, Bio = "ararara", Id = 1 });
+                context.SaveChangesAsync();
+                return Ok("successfully added " + name + "to the users");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        //this doesnt work
+        [Route("GetUsers")]
+        [HttpGet]
+        public async Task<ActionResult<User>> GetUsers()
+        {
+            try
+            {
+                return Ok(context.Users.Where(u => u.Id == 1).FirstOrDefault());
+            }
+            catch (Exception ex) { return BadRequest(ex); }
+        }
+
     }
 }
